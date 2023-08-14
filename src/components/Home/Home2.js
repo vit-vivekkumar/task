88c10bf86708cb6d3b1dd738e27f4a79b7533e5b
vivek kumar <vivek964800@gmail.com>
2023-08-14 09:32:03 +0530

    changes

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -63,7 +63,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="#"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -73,7 +73,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/vivek-vishwakarma-15680116b/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -83,7 +83,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/vivek_vishwakarma909/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
